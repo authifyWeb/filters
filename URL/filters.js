6939829f041ff614e097b8eb464720ca11abba24
webVerts <webverts@protonmail.com>
2024-01-27 01:50:39 +0530

    Add myportfolio.com
    Adobe Portfolio
    fixes :https://github.com/authifyWeb/filters/issues/67

--- URL/filters.js
+++ URL/filters.js
@@ -332,7 +332,9 @@ function filtering(url, href, origin, hostname, protocol, pathname, search, doma
       link=hostname;
       var output=compare(link,href); return output;
     }
-    
+    else if(domain=="myportfolio.com"){
+        link=hostname; console.log(link); var output=compare(link,href); return output;
+    }
     else if (hostname == "addons.mozilla.org")
     /*Once the above condition is true, the function replaces the URL language to the the default en-US. This is used since mozilla supports multiple languages and the url structure is directly based on user language.  */
     {
